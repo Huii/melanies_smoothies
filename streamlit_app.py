fd0bad9d49cfa63836edc37d916115a6cfaeb66d
Huii <gromobir@web.de>
2024-04-03 14:45:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 st.title(":cup_with_straw: Customize")
 st.write("""Choose the fruits""")
